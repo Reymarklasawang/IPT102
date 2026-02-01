ad4cf08a2b610ea0adcf58aea2ef30ab2bed5b44
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2026-02-01 19:59:02 +0800

    Add files via upload

---
+++ IPT102LasawangData/Updatefaculty.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Updatefaculty]
+@Id NVARCHAR(40) = NULL,
+	@Code NVARCHAR(24) = NULL,
+	@Title NVARCHAR(128) = NULL,
+	@Credit NUMERIC(5, 2) = NULL
+AS
+BEGIN
+	-- checks
+	-- Return;
+
+	UPDATE [dbo].[Faculty]
+	SET [Code] = @Code,
+		[Title] = @Title,
+		[Credit] = @Credit
+	WHERE [Id] = @Id;
+END
