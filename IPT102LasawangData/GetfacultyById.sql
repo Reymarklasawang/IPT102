bd57f7eda90ba866d53be4deb94dcd2ca9524c8b
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2026-02-01 20:44:17 +0800

    Remove GetfacultyById stored procedure

--- IPT102LasawangData/GetfacultyById.sql
+++ IPT102LasawangData/GetfacultyById.sql
@@ -1,13 +1,1 @@
-﻿CREATE PROCEDURE [dbo].[GetfacultyById]
-	@Code NVARCHAR(24) = NULL
-AS
-BEGIN
-	-- checks
 
-	SELECT a.[Id] AS FacultyId,
-		a.[Code] AS FacultyCode,
-		a.[Title] AS FacultyTitle,
-		a.[Credit] AS FacultyCredit
-	FROM [dbo].[Faculty] AS a
-	WHERE a.[Code] = @Code
-END
