680fa462dfed927f2cb60bc378c7cc8d89bcf36c
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2026-02-01 21:02:13 +0800

    Add files via upload

---
+++ IPT102LsawangWpfApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IPT102LsawangWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
