96d8e5d8dbf113d40d5b599647b9ccc4605772e5
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2026-02-01 20:48:36 +0800

    Add files via upload

---
+++ IPT102LasawangData/Tables/Faculty.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Faculty]
+(
+	[Id] NVARCHAR(40) NOT NULL,
+	[Code] NVARCHAR(24),
+	[Title] NVARCHAR(128),
+	[Credit] NUMERIC(5, 2),
+
+	CONSTRAINT PK_Faculty_ID PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Faculty_Code] ON [dbo].[Faculty] ([Code] ASC)
+WHERE [Code] IS NOT NULL;
