9fdded979bd4a1961511570de09d3ddbb4e81910
Lasawang04 <144080214+Reymarklasawang@users.noreply.github.com>
2026-02-01 20:43:39 +0800

    Remove Deletefaculty stored procedure

--- IPT102LasawangData/Deletefaculty.sql
+++ IPT102LasawangData/Deletefaculty.sql
@@ -1,10 +1,1 @@
-﻿CREATE PROCEDURE [dbo].[Deletefaculty]
-	@Id NVARCHAR(40) = NULL
-AS
-BEGIN
-	
-	IF LEN(LTRIM(RTRIM(COALESCE(@Id, N'')))) = 0 RETURN;
 
-	DELETE FROM [dbo].[Faculty]
-	WHERE [Id] = @Id;
-END
